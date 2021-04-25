b8bb7bfc1380610cb813f6a691cb0eff40c5e880
Jefferson Owle <hootalicious@gmail.com>
2021-04-25 19:29:52 -0400

    Saving new code practice

--- src/Addition.java
+++ src/Addition.java
@@ -2,13 +2,29 @@ import java.util.Scanner;
 
 public class Addition {
 
+	static int addTwoNumbers(int x, int y){
+	     return x + y;
+	}
+	
+	
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
 		Scanner userInput = new Scanner(System.in);
-		System.out.print("enter a number: ");
-		int number = userInput.nextInt();
-		System.out.println("the number is " + number);
-
+		
+		System.out.print("Number of operations? ");
+		int t = userInput.nextInt();
+		
+		
+		for(int i=1; i<=t; i++){
+			System.out.print("enter first number: ");
+			int num1 = userInput.nextInt();
+			System.out.print("enter second number: ");
+			int num2 = userInput.nextInt();
+			
+			System.out.println("the sum is " + addTwoNumbers(num1, num2));
+		
+		 }
+		
 	}
 
 }
--- src/InputOutput.java
+++ src/InputOutput.java
@@ -6,7 +6,7 @@ public class InputOutput{
 		@SuppressWarnings("resource")
 		Scanner userInput = new Scanner(System.in);
 		System.out.print("enter a number: ");
-		int number = userInput.nextInt();
+		int number = userInput.nextInt();   // OR use a string: String number = userInput.nextLine();
 		System.out.println("the number is " + number);
 
 	}
