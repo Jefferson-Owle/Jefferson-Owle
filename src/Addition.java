86a07f1748c56830ebbac4f4ce10de8a062dcbdb
Jefferson Owle <hootalicious@gmail.com>
2021-04-25 17:40:25 -0400

    First try with keyboard input/output

---
+++ src/Addition.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("enter a number: ");
+		int number = userInput.nextInt();
+		System.out.println("the number is " + number);
+
+	}
+
+}
